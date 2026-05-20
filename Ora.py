ffce680d73c29ddd7c63606b5134514ddf8d6959
Gyozod <gyozidav@gmail.com>
2026-05-20 18:54:33 +0200

    Nagymutato kesz

--- Ora.py
+++ Ora.py
@@ -2,5 +2,9 @@ import turtle
 
 turtle.pencolor("blue")
 turtle.goto(0, 100)
-turtle.forward(30)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(10)
 turtle.done()
