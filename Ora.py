216ee95a751c4582d1af4ea9a7fb3628a49490a9
doczybarni <doczybarni@gmail.com>
2026-05-20 19:36:10 +0200

    piros mutato kesz

--- Ora.py
+++ Ora.py
@@ -12,5 +12,13 @@ turtle.penup()
 turtle.goto(0, 0)
 turtle.pendown()
 
+turtle.pencolor("red")
+turtle.left(-130)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(100)
+
 
 turtle.done()
